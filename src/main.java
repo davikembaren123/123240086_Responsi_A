6f1f83ac811b694f9b0e81ae841ce4b2b990d4e2
davikembaren123 <daviergana06@gmail.com>
2026-05-18 12:23:15 +0700

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemhotel0;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class main {
+
+    public static void main(String[] args) {
+        new view.LoginFrame();
+        setVisible(true);
+    }
+}
